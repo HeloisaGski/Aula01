8e3dd3505e965bd11f4150eb549f4374db20a8ce
HeloisaGski <146874643+HeloisaGski@users.noreply.github.com>
2024-08-12 18:34:13 -0300

    atualizando o Readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Aula01
- Repositório para AULA01 de LP
+# Aula 01
+ Repositório para Aula 01 de Laboratório de Programação. O uso da licença e README foram por instrução do curso de Git e GitHub do Gustavo Guanabara, com o curso em vídeo. 
--- obj/Debug/net8.0/Aula01.AssemblyInfo.cs
+++ obj/Debug/net8.0/Aula01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc9cb91ca499dc876287e7d1f2d17b84bcc81db3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
